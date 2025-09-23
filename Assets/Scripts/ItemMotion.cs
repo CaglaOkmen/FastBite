3a99e44c0b229de1ae23eb658f1ae47596acef29
Cagla Okmen <200101069@ogrenci.yalova.edu.tr>
2025-09-23 03:37:05 +0300

    Update ItemMotion.cs

--- Assets/Scripts/ItemMotion.cs
+++ Assets/Scripts/ItemMotion.cs
@@ -1,32 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using static UnityEditor.Progress;
 public class ItemMotion : MonoBehaviour
 {
     float speed = 2f;
-    GameObject[] gameObjects = new GameObject[0];
+    Rigidbody2D[] rb;
+    List<Vector3> mov = new List<Vector3>();
 
     private void Update()
     {
-        if (gameObjects.Length == 0)
+        if (rb.Length == 0)
         {
             print("boþ");
         }
         else
         {
-            for (int i = 0; i < gameObjects.Length; i++)
+            for (int i = 0; i < rb.Length; i++)
             {
-
-                Vector3 movement = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-                gameObjects[i].transform.Translate(movement * speed * Time.deltaTime);
+                // kenara çarpýp yön deðiþtirilecek
+                rb[i].AddForce(speed * mov[i]);
                 print("hareket var");
             }
         }
     }
     public void randomItemMotion(GameObject[] items)
     {
-        gameObjects = items;
+        rb = new Rigidbody2D[items.Length];
+        mov.Clear();
+
+        for (int i = 0; i < items.Length; i++)
+        {
+            items[i] = Instantiate(items[i], transform);
+            rb[i] = items[i].GetComponent<Rigidbody2D>();
+            mov.Add(new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f));
+            print("rb & mov uretildi");
+        }
     }
 }
